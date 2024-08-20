4424f16d786fa7e804950eeb4d0ae6bf7b04d8d7
basilahamed07 <basilahamed46@gmail.com>
2024-08-20 11:15:15 +0530

    task completed

--- project_1/app_1/views.py
+++ project_1/app_1/views.py
@@ -5,25 +5,20 @@ from .models import *
 def home_page(request):
     introduction_data = Introduction.objects.all()[0]
     print(introduction_data.first_name)
-    posts = Posts.objects.all().order_by("time")[0:3]
+    posts = Posts.objects.all().order_by("-time").values()[0:3]
     counts_landing = posts.count()
-    count_1 = 1
-    count_2 = 2
-    count_3 = 3
+    # count_1 = 1
+    # count_2 = 2
+    # count_3 = 3
     print(posts)
 
     return render(request,"app_1/landing.html",{"introduction_data":introduction_data,
-                                                "three_data":posts,
-                                                "landing_count":counts_landing,
-                                                "count1":count_1,
-                                                "count2":count_2,
-                                                "count3":count_3,
-                                                
-                                                })
+                                                "data":posts,
+                                                "landing_count":counts_landing,})
 
 def all_post(request):
     posts = Posts.objects.all()
-    return render(request,"app_1/all_post.html",{"post":posts})
+    return render(request,"app_1/all_post.html",{"data":posts})
 
 def details(request,details):
     posts = Posts.objects.get(slug=details)
